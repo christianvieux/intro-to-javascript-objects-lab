91a56cd1bc071efa71019d071bdca6973a8b2253
christianvieux-GA <christianvieux.dev@gmail.com>
2025-02-27 14:53:49 -0500

    Included Level Up Exercises - Didn't see it (again)

--- app.js
+++ app.js
@@ -171,7 +171,7 @@ Solve Exercise 12 here:
 
 game.gyms.forEach(gym => {
     if (gym.difficulty < 6) {
-        gym.complete = true; // Instructions says complete and not "completed", so yeah...
+        gym.complete = true; // Instructions says "complete" and not "completed", so yeah...
     };
 });
 
@@ -246,7 +246,7 @@ Solve Exercise 15 here:
 
 game.gyms.forEach(gym => {
     if (gym.difficulty < 8) {
-        gym.complete = true; // Instructions says complete and not "completed", so yeah...
+        gym.complete = true; // Instructions says "complete" and not "completed", so yeah...
     };
 });
 
@@ -261,3 +261,174 @@ Solve Exercise 16 here:
 */
 
 console.log(game);
+
+
+/*
+Exercise 17
+1. Arrange the Pokémon in `game.party` by their HP. The one with the highest HP should come first.
+2. You'll need to use the `.sort()` method. How does the compare function work in sorting numbers?
+
+
+Solve Exercise 17 here:
+*/
+
+
+game.party.sort((a, b) => b.hp - a.hp);
+
+
+
+/*
+Exercise 18
+Add a new property to the `game` object called `collection` and initialize its value to an empty array.
+
+Copy the `catchPokemon` method you wrote in Exercise Twelve and paste it below. Modify it so that:
+  - Ensure that no more than six Pokemon can be in the party at any time. 
+    Excess Pokemon should be placed in the `game.collection` array.
+  - It's up to you how to distribute Pokemon in a situation where more than six 
+    would be placed into the `game.party` array.
+
+Again, for this exercise, it's okay to have a negative number of pokeballs.
+
+After updating the method, use it by calling it and passing in a pokemon object of your choice from the `pokemon` data to catch it.
+
+Also, log the `game.items` array to confirm that the pokeball quantity is being decremented.
+
+Solve Exercise 18 here:
+*/
+
+
+game.collection = [];
+
+game.catchPokemon = (pokemonObj) => {
+    const isPartyFull = game.party.length >= 6;
+    
+    if (isPartyFull) {
+        game.collection.push(pokemonObj);
+    } else {
+        game.party.push(pokemonObj);
+    };
+
+    game.items.find(item => item.name == "pokeball").quantity -= 1;
+};
+
+
+/*
+Exercise 19
+Copy the `catchPokemon` method that you just wrote above, and paste it below. The time has come to make it so that we cannot catch a Pokemon when we do not have any pokeballs to catch it with. 
+
+Modify the method so that if there are no pokeballs a message will be displayed that there are not enough pokeballs to catch the desired Pokemon.
+
+Also, ensure that the Pokemon isn't added to the `game.party` or the `game.collection`.
+
+Solve Exercise 19 here:
+*/
+
+game.catchPokemon = (pokemonObj) => {
+  const pokeball = game.items.find((item) => item.name == "pokeball");
+  const hasEnoughPokeballs = pokeball.quantity > 0;
+  
+  if (hasEnoughPokeballs) {
+    const isPartyFull = game.party.length >= 6;
+    if (isPartyFull) {
+      game.collection.push(pokemonObj);
+    } else {
+      game.party.push(pokemonObj);
+    }
+
+    pokeball.quantity -= 1;
+  } else {
+    console.log("Not enough pokeballs :(");
+  }
+};
+
+
+
+/*
+Exercise 20
+Copy the `catchPokemon` method that you just wrote above, and paste it below. Modify is so that you can just pass in the name of a Pokemon instead of an entire object, and the method will look up the Pokemon from the data set for you.
+
+The string passed in should be allowed to be any case (for example, if the string 'PiKacHU' is passed to the function, it should match to 'Pikachu' in the data set). 
+
+If there is not a match, then return a string noting that the selected Pokemon does not exist. Ensure you do not decrement the pokeball count if an invalid Pokemon name is passed in, and also ensure that the Pokemon isn't added to the `game.party` or the `game.collection`.
+
+Solve Exercise 20 here:
+*/
+
+
+game.catchPokemon = (pokemonName) => {
+  if (!pokemonName) {
+    return console.log("The pokemon's name has not been provided!");
+  }
+
+  const nameOfPokemonToCatch = pokemonName.toLowerCase();
+  const pokemonExists = pokemon.find(
+    (pokemon) => pokemon.name.toLowerCase() == nameOfPokemonToCatch
+  );
+
+  if (pokemonExists) {
+    const pokeball = game.items.find((item) => item.name == "pokeball");
+    const hasEnoughPokeballs = pokeball.quantity > 0;
+
+    if (hasEnoughPokeballs) {
+      const isPartyFull = game.party.length >= 6;
+      if (isPartyFull) {
+        game.collection.push(pokemonExists);
+      } else {
+        game.party.push(pokemonExists);
+      }
+
+      pokeball.quantity -= 1;
+
+      console.log(`${nameOfPokemonToCatch} has been caught!`);
+    } else {
+      console.log("Not enough pokeballs :(");
+    }
+  } else {
+    console.log(`The pokemon ${pokemonName} does not exist :(`);
+  }
+};
+
+
+
+/*
+Exercise 21
+Dynamically construct an object with the existing `pokemon` data sorted by the different pokemon types. The object will have this structure:
+
+{
+  grass: [
+    { number: 1, name: 'Bulbasaur', type: 'grass', hp: 45, starter: true },
+    { number: 2, name: 'Ivysaur', type: 'grass', hp: 60, starter: false },
+    { number: 3, name: 'Venusaur', type: 'grass', hp: 80, starter: false },
+    * more grass type Pokemon objects...
+  ],
+  fire: [
+    { number: 4, name: 'Charmander', type: 'fire', hp: 39, starter: true },
+    * more fire type Pokemon objects...
+  ],
+  water: [
+    * water type Pokemon objects...
+  ],
+  * etc... until there is an array for every Pokemon type!
+}
+
+Log the object when it's constructed.
+
+Solve Exercise 21 here:
+*/
+const getSortedPokemons = function () {
+  const sortedPokemons = {};
+
+  pokemon.forEach((pokemonObj) => {
+    if (!sortedPokemons[pokemonObj.type]) {
+      sortedPokemons[pokemonObj.type] = [];
+    }
+
+    sortedPokemons[pokemonObj.type].push(pokemonObj);
+  });
+
+  return sortedPokemons;
+};
+
+const sortedPokemons = getSortedPokemons();
+
+console.log(sortedPokemons);
